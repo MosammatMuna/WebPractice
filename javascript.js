96e6817d601b15e3b87ffe3c315dbeba778900f6
Mosammat Muna <munamosammat@gmail.com>
2018-11-03 20:09:47 +0000

    add javascript and get involved.

---
+++ javascript.js
@@ -0,0 +1,19 @@
+//javascript.js
+
+var slideIndex = 0;
+showSlides();
+function showSlides()
+  {
+      var slides=document.getElementsByClassName("myimage");
+      for (var i=0;i<slides.length;i++)
+       {
+           slides[i].style.display="none";
+       }
+      slideIndex++;
+      if(slideIndex >=slides.length)
+       {
+         slideIndex=0;
+       }
+       slides[slideIndex].style.display="block";
+       setTimeout(showSlides,4000);
+  }
